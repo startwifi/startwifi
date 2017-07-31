b76323d3e66d6d7488dd7f2cce6af42a40715b93
Max Grechko <gremax@gremax.me>
2017-07-31 22:42:04 +0300

    Add styles and images

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,15 @@ import configureStore from './store'
 import routes from './routes'
 import './index.css'
 
+import jquery from 'jquery'
+window.jQuery = jquery;
+
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './../node_modules/font-awesome/css/font-awesome.css'
+import './../public/styles/style.css'
+
+require('bootstrap');
+
 export const store = configureStore()
 const history = syncHistoryWithStore(browserHistory, store)
 
